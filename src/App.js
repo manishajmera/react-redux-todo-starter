ece19ddb1615af17d960027c6b23cce6421dd4cf
manishajmera27 <manish.jain@girnarsoft.com>
2019-05-30 01:22:33 +0530

    MockDownProject

--- src/App.js
+++ src/App.js
@@ -1,29 +1,111 @@
-import React, { Component } from 'react';
+import React from "react";
 import './App.css';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import {addToList} from './actions/todo';
 
-import TodoInput from './components/TodoInput'
-import TodoList from './components/TodoList/TodoList'
 
-export class App extends Component {
+export default class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {}
+    }
+    render () {
+        return (
+            <div>
+                {this.state.string && <div class="sentence" dangerouslySetInnerHTML={{__html:this.state.string}}/>}
+                {!this.state.string && <div>
+                    <h1 className="heading">MarkDownScreen</h1>
+                    <div>
+                        <span>Instructions</span>
+                        <ul>
+                            <li>*This makes the text bold*</li>
+                            <li>_This underlines the text_</li>
+                            <li>^This increases the font size^</li>
+                            <li>[To make a link]</li>
 
-  mySubmit(values) {
-    this.props.addToList(values.title)
-  }
+                        </ul>
+                    </div>
+                    <form onSubmit={this.onSubmit.bind(this)} className="create-todo-form">
+                        <textarea  placeholder="Enter Your sentence here" ref="taskMessage" autoFocus/>
+                        <button className="primaryButton">Add</button>
+                    </form>
+                </div>
+                }
+            </div>
+        );
+    }
+    onSubmit (e) {
+        this.createTask(this.refs.taskMessage.value);
+        this.refs.taskMessage.value = "";
+        e.preventDefault();
+    }
 
-  render() {
-    return (
-      <div>
-        <TodoInput onSubmit={this.mySubmit.bind(this)}/>
-        <TodoList/>
-      </div>
-    );
-  }
+    createTask (str) {
+	var bold = -1;
+	var underline = -1;
+	var increaseFontSize =-1;
+	var leftSideBracket = -1;
+    var incresedCharacterCount=0;
+	var tempStr  = str;
+        for(var i=0 ;i< str.length;i++){
+            if(str[i]=="*"){
+		if(bold!=-1){
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "</b>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=3
+			bold = -1;
+		}else{
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "<b>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=2
+			bold = i;
+		}
+
+            }else if(str[i]=="_"){
+		if(underline!=-1){
+			console.log("")
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "</u>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=3
+			underline = -1;
+		}else{
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "<u>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=2
+			underline = i;
+		}
+            }else if(str[i]=="^"){
+		if(increaseFontSize!=-1){
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "</h3>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=4
+			increaseFontSize = -1;
+		}else{
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "<h3>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=3
+			increaseFontSize = i;
+		}
+            }else if(str[i] == "[" && leftSideBracket==-1){
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "<a href='#'>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=11
+			leftSideBracket = i;
+		}else if(str[i] == "]" && leftSideBracket!=-1){
+			tempStr = tempStr.substring(0,i+incresedCharacterCount) + "</a>" + tempStr.substring(i+1+incresedCharacterCount,tempStr.length)
+			incresedCharacterCount+=3
+			leftSideBracket = -1;
+		}	
+        }
+	//negative scenrios
+	if(bold!=-1){
+		let last_index = tempStr.lastIndexOf("<b>");
+		tempStr = tempStr.substring(0,last_index) + "*" + tempStr.substring(last_index+3,tempStr.length)
+	}
+	if(underline!=-1){
+		let last_index = tempStr.lastIndexOf("<u>");
+		tempStr = tempStr.substring(0,last_index) + "_" + tempStr.substring(last_index+3,tempStr.length)
+	}
+	if(increaseFontSize!=-1){
+		let last_index = tempStr.lastIndexOf("<h3>");
+		tempStr = tempStr.substring(0,last_index) + "^" + tempStr.substring(last_index+4,tempStr.length)
+	}
+	if(leftSideBracket!=-1){
+		let last_index = tempStr.lastIndexOf("<a>");
+		tempStr = tempStr.substring(0,last_index) + "[" + tempStr.substring(last_index+11,tempStr.length)
+	}
+        this.setState({ string: tempStr });
+    }
 }
-
-const mapDispatchToProps = dispatch => bindActionCreators({ addToList }, dispatch)
-
-export default connect(null, mapDispatchToProps)(App);
